2b4ad1ff041fd2a94bd3f9d3694439bfed1c349d
santoshchem51 <net.santosh51@gmail.com>
2018-01-05 19:32:17 -0600

    added route constraint to enable api versioning

---
+++ WebApi/SampleWebApi/Helper/VersionConstraint.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Http.Routing;
+
+namespace SampleWebApi.Helper
+{
+    internal class VersionConstraint : IHttpRouteConstraint
+    {
+        public const string VersionHeaderName = "api-version";
+
+        public const int DefaultVersion = 1;
+
+        public int AllowedVersion { get; private set; }
+        public VersionConstraint(int allowedVersion)
+        {
+            AllowedVersion = allowedVersion;
+        }
+
+        public bool Match(HttpRequestMessage request, IHttpRoute route, 
+            string parameterName, IDictionary<string, object> values, HttpRouteDirection routeDirection)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int? GetVersionFromCustomContentType(HttpRequestMessage request)
+        {
+            string versionAsString = null;
+
+            // get the accept header.
+
+            var mediaTypes = request.Headers.Accept.Select(h => h.MediaType);
+            string matchingMediaType = null;
+            // find the one with the version number - match through regex
+            Regex regEx = new Regex(@"application\/vnd\.expensetrackerapi\.v([\d]+)\+json");
+
+            foreach (var mediaType in mediaTypes)
+            {
+                if (regEx.IsMatch(mediaType))
+                {
+                    matchingMediaType = mediaType;
+                    break;
+                }
+            }
+
+            if (matchingMediaType == null)
+                return null;
+
+            // extract the version number
+            Match m = regEx.Match(matchingMediaType);
+            versionAsString = m.Groups[1].Value;
+
+            // ... and return
+            int version;
+            if (versionAsString != null && Int32.TryParse(versionAsString, out version))
+            {
+                return version;
+            }
+
+            return null;
+        }
+
+        private int? GetVersionFromCustomRequestHeader(HttpRequestMessage request)
+        {
+            string versionAsString;
+            IEnumerable<string> headerValues;
+            if (request.Headers.TryGetValues(VersionHeaderName, out headerValues) && headerValues.Count() == 1)
+            {
+                versionAsString = headerValues.First();
+            }
+            else
+            {
+                return null;
+            }
+
+            int version;
+            if (versionAsString != null && Int32.TryParse(versionAsString, out version))
+            {
+                return version;
+            }
+
+            return null;
+        }
+
+
+    }
+}
